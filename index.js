8778883be4ffb0fd123305f21a9e4428ff5dc333
Dimitar Iliev <DIliev@centermine.com>
2016-02-09 12:45:42 +0200

    use browserify transform to compile files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var hb = require("handlebars");
-require('./index.hbs.js');
+/*var hb = require("handlebars");
+require('./index.hbs.js');*/
+
+var tp = require('./templates/index.hbs');
 
 module.exports = {
 	render: function() {
-		return hb.templates['index.hbs']();
+		return tp();
 	}
 }
